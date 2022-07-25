a4a1df19be366c4da0e59153edff1607a0e23c5d
Ha Tuan Anh <109954301+tuananhha42@users.noreply.github.com>
2022-07-25 17:22:14 +0700

    abc

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Xin chào Linh iuuu!' ,
+    introDesc: `Anh có cái này mún hỏi iemmm👉👈`,
+    btnIntro: 'oke',
+    title: 'Em iuuu anh hummm? 😙',
+    desc: 'Em  không trả lời hoặc thoát ra, tức là em có iu đóa nhoaa :vvvv :> ',
+    btnYes: 'Iuu nhắmmmm',
+    btnNo: 'Anh mơ tiếp đi ',
+    question:'Sao lại thích mình nhỉ? <3',
+    btnReply: 'Gửi cho mềnh <3',
+    reply: 'Tại vì anh đẹp trai hihi 😚',
+    mess: 'Anh biết mà 🥰. Love you 3000 😘😘',
+    messDesc: 'Baby cho anh hun mụt cáiii',
+    btnAccept: 'Okiiiii lun <3',
+    FbLink: 'https://www.facebook.com/watch/?ref=search&v=2157057677732310&external_log_id=a3f39fa7-8316-4684-bc20-070ebc2b8a16&q=anh%20y%C3%AAu%20em%20n%E1%BA%AFm' 
+}
